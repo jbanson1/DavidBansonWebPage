f5d380a3a08693816dc1adf1c9d6e73c39cbba1f
Joseph Banson <33466255+jbanson1@users.noreply.github.com>
2025-10-25 08:19:21 +0100

    first commit

---
+++ src/lib/constants.ts
@@ -0,0 +1,12 @@
+export const LINKS = {
+  github: "https://github.com/daveittt",
+  linkedin: "https://www.linkedin.com/in/davidbanson/",
+  medium: "https://medium.com/@daveitt",
+  email: "dbanson@hotmail.com<"
+};
+
+export const loaderAnimation = [
+  ".loader",
+  { opacity: [1, 0], pointerEvents: "none" },
+  { easing: "ease-out" },
+];
